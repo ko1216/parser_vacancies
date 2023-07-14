54a49db98d7e065f6df11aa048ec1aaa74104c30
korchaginks <kkonstantinserg@gmail.com>
2023-07-14 21:55:04 +0300

    - исправил метод get_vacancies_by_salary: добавил параметр в записи файла, чтобы энкодинг передавал символы кириллицы не в виде байтов -дописал докстринги

--- src/class_json.py
+++ src/class_json.py
@@ -1,7 +1,6 @@
 import json
 from abc import ABC, abstractmethod
 from src.class_vacancy import Vacancy
-from src.class_API import HeadHunterAPI, SuperJobAPI
 
 
 class Json(ABC):
@@ -21,37 +20,49 @@ class Json(ABC):
 class JsonSaver(Json):
     data = []
 
+    def __init__(self, filename='vacancies_request_file'):
+        self.filename = filename + '.json'
+
     def add_vacancy(self, vacancies: list):
+        """
+        Метод инициализирует все полученные вакансии в список экземпляра класса для дальнейшей обработки
+        :param vacancies: список инициализированных экземпляров класса Vacancy
+        """
         self.data.clear()
 
         Vacancy.instantiate_from_list(vacancies)
         for vacancy in Vacancy.all_vacancies:
             self.data.append(vacancy)
 
-    def get_vacancies_by_salary(self, salary):
+    def get_vacancies_by_salary(self, salary: int):
+        """
+        Метод создает промежуточный список для иттерации по экземплярам вакансии, чтобы сравнить их с параметром salary
+        Метод записывает файл с вакансиями, которые соответствуют переданной зарплате
+        :param salary: целое число - зарплата
+        :return: записывает файл с вакансиями
+        """
         new_data = []
 
         for vacancy in self.data:
-            # vacancy_for_comparison = Vacancy({'name': None, 'link': None, 'candidat': None, 'area': {'name': None},
-            #                                   'payment_from': None, 'payment_to': salary})
             if vacancy.get_max_salary() >= salary:
-                new_data.append(vacancy)
+                new_data.append({
+                    'Вакансия': vacancy.name,
+                    'Ссылка на вакансию': vacancy.url,
+                    'Зарплата от': vacancy.salary_from,
+                    'Зарплата до': vacancy.salary_to,
+                    'Город': vacancy.city,
+                    'Информация': vacancy.info,
+                })
 
-        return new_data
+        with open(self.filename, 'w', encoding='utf-8') as f:
+            json.dump(new_data, f, ensure_ascii=False)
 
     def delete_vacancy(self, vacancy):
+        """
+        Метод можно использвать, как удаление вакансий или файлов в зависимости от реализации. В моей реализации юзер
+        получает суженный список вакансий (можно сузить еще по сортирвке топа по ЗП) по заданным и предустановленным
+        требованиям
+        :param vacancy: -
+        :return: -
+        """
         pass
-
-
-hh_api = HeadHunterAPI()
-superjob_api = SuperJobAPI()
-
-hh_api.get_vacancies("Python")
-superjob_api.get_vacancies("Python")
-
-vacancies_list = hh_api.vacancies_list + superjob_api.vacancies_list
-
-json_s = JsonSaver()
-json_s.add_vacancy(vacancies_list)
-print(json_s.get_vacancies_by_salary(100000))
-# print(json_s.data[0].salary, json_s.data[-1].salary)
